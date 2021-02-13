f13af4ff710fc3396b24138840a8b17651d6e1e5
Bartek Pacia <barpac02@gmail.com>
2021-02-13 13:09:16 +0100

    make it async

--- master.go
+++ master.go
@@ -1,42 +1,38 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
 	"log"
+	"os"
 	"os/exec"
 )
 
 func main() {
 	command := exec.Command("lidar-scan", "/dev/ttyUSB0")
 
-	out, err := command.CombinedOutput()
+	stdout, err := command.StderrPipe()
 	if err != nil {
-		log.Fatalln("master: failed to run command:", err)
+		log.Fatalln("master: failed to connect to command's stdout:", err)
 	}
 
-	fmt.Println("master: begin subprogram output")
-	fmt.Println(string(out))
-	fmt.Println("master: end subprogram output")
+	scanner := bufio.NewScanner(stdout)
+	go func() {
+		fmt.Println("in goroutine")
+		for scanner.Scan() {
+			fmt.Println("in scan")
+			fmt.Printf("\t > %s\n", scanner.Text())
+		}
+	}()
 
-	// stdout, err := command.StdoutPipe()
-	// if err != nil {
-	// 	log.Fatalln("master: failed to connect to command's stdout:", err)
-	// }
+	err = command.Start()
+	if err != nil {
+		log.Fatalln("maste: failed to run command:", err)
+	}
 
-	// scanner := bufio.NewScanner(stdout)
-	// go func() {
-	// 	fmt.Println("in goroutine")
-	// 	for scanner.Scan() {
-	// 		fmt.Println("in scan")
-	// 		fmt.Printf("\t > %s\n", scanner.Text())
-	// 	}
-	// }()
-
-	// CUT
-
-	// err = command.Wait()
-	// if err != nil {
-	// 	fmt.Fprintln(os.Stderr, "master: failed to wait for command", err)
-	// 	return
-	// }
+	err = command.Wait()
+	if err != nil {
+		fmt.Fprintln(os.Stderr, "master: failed to wait for command", err)
+		return
+	}
 }
