bf873e235c01324dde93aae745676b151686796c
Bartek Pacia <barpac02@gmail.com>
2021-02-13 19:48:10 +0100

    read from stdin and stdout simultaneously

--- master.go
+++ master.go
@@ -11,28 +11,33 @@ import (
 func main() {
 	command := exec.Command("lidar-scan", "/dev/ttyUSB0")
 
-	stdout, err := command.StderrPipe()
+	cmdStdout, err := command.StdoutPipe()
 	if err != nil {
 		log.Fatalln("master: failed to connect to command's stdout:", err)
 	}
 
-	scanner := bufio.NewScanner(stdout)
-	go func() {
-		fmt.Println("in goroutine")
-		for scanner.Scan() {
-			fmt.Println("in scan")
-			fmt.Printf("\t > %s\n", scanner.Text())
-		}
-	}()
+	cmdStderr, err := command.StderrPipe()
+	if err != nil {
+		log.Fatalln("master: failed to connect to command's stderr:", err)
+	}
+
+	go scan(bufio.NewScanner(cmdStdout))
+	go scan(bufio.NewScanner(cmdStderr))
 
 	err = command.Start()
 	if err != nil {
-		log.Fatalln("maste: failed to run command:", err)
+		log.Fatalln("master: failed to run command:", err)
 	}
 
 	err = command.Wait()
 	if err != nil {
 		fmt.Fprintln(os.Stderr, "master: failed to wait for command", err)
 		return
+	}
+}
+
+func scan(scanner *bufio.Scanner) {
+	for scanner.Scan() {
+		fmt.Printf("master: from command: %s\n", scanner.Text())
 	}
 }
