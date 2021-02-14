c1e9d1b2b7558986ed8a7ea019b5ed469f9fc886
Bartek Pacia <barpac02@gmail.com>
2021-02-14 01:17:47 +0100

    simplify a bit

--- roverd.go
+++ roverd.go
@@ -2,15 +2,20 @@ package main
 
 import (
 	"fmt"
-	"html"
 	"log"
 	"net/http"
+	"os"
+	"os/signal"
 
 	"github.com/sevlyar/go-daemon"
 )
 
+var (
+	sigChan = make(chan os.Signal, 1)
+)
+
 // To terminate the daemon use:
-//  kill `cat roverd.pid`
+// kill `cat roverd.pid`
 func main() {
 	ctx := &daemon.Context{
 		PidFileName: "roverd.pid",
@@ -19,7 +24,6 @@ func main() {
 		LogFilePerm: 0640,
 		WorkDir:     "./",
 		Umask:       027,
-		Args:        []string{"[go-daemon sample]"},
 	}
 
 	daemon, err := ctx.Reborn()
@@ -33,8 +37,17 @@ func main() {
 
 	log.Print("- - - - - - - - - - - - - - -")
 	log.Print("daemon started")
+	signal.Notify(sigChan, os.Interrupt)
+	go listenInterrupt()
 
 	serveHTTP()
+}
+
+func listenInterrupt() {
+	for sig := range sigChan {
+		log.Printf("dameon stopped by %s", sig.String())
+		os.Exit(0)
+	}
 }
 
 func serveHTTP() {
@@ -42,7 +55,7 @@ func serveHTTP() {
 	http.ListenAndServe("127.0.0.1:8080", nil)
 }
 
-func httpHandler(w http.ResponseWriter, r *http.Request) {
-	log.Printf("request from %s: %s %q", r.RemoteAddr, r.Method, r.URL)
-	fmt.Fprintf(w, "hello from roverd: %q", html.EscapeString(r.URL.Path))
+func httpHandler(w http.ResponseWriter, req *http.Request) {
+	log.Printf("request from %s: %s %q", req.RemoteAddr, req.Method, req.URL)
+	fmt.Fprintln(w, "hello from roverd!")
 }
