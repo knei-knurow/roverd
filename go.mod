4d59ff45b6f739a00df32ee26bd20d298d2ece0b
Bartek Pacia <barpac02@gmail.com>
2021-06-15 01:19:22 +0200

    go (forward/backward) work with roverctl

--- go.mod
+++ go.mod
@@ -3,7 +3,8 @@ module github.com/knei-knurow/roverd
 go 1.16
 
 require (
-	github.com/kardianos/osext v0.0.0-20190222173326-2bc1f35cddc0 // indirect
-	github.com/sevlyar/go-daemon v0.1.5
+	github.com/joho/godotenv v1.3.0
+	github.com/knei-knurow/frames v1.0.2
+	github.com/tarm/serial v0.0.0-20180830185346-98f6abe2eb07
 	golang.org/x/sys v0.0.0-20210611083646-a4fc73990273 // indirect
 )
--- roverd.go
+++ roverd.go
@@ -1,47 +1,109 @@
 package main
 
 import (
+	"encoding/json"
+	"flag"
+	"fmt"
+	"io"
 	"log"
+	"net/http"
 	"os"
-	"os/signal"
-	"syscall"
+	"strconv"
 
-	"github.com/sevlyar/go-daemon"
+	"github.com/joho/godotenv"
+	"github.com/knei-knurow/frames"
+	"github.com/tarm/serial"
 )
 
-var signals = make(chan os.Signal, 1)
+var (
+	verbose bool
+)
+
+var (
+	// Port on which the roverd will listen for requests
+	port string
+
+	// Port with the device controlling motors
+	movePort io.ReadWriteCloser
+)
 
 func init() {
 	log.SetFlags(0)
 	log.SetPrefix("roverd: ")
+
+	flag.BoolVar(&verbose, "verbose", false, "print verbose output")
+	flag.Parse()
+
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatalln("failed to load .env file:", err)
+	}
+
+	port = os.Getenv("LISTEN_PORT")
+
+	movePortName := os.Getenv("MOVE_PORT_NAME")
+	baudRate, err := strconv.Atoi(os.Getenv("MOVE_BAUD_RATE"))
+	if err != nil {
+		log.Fatalf("cannot read baud rate: %v\n", err)
+	}
+
+	config := &serial.Config{
+		Name: movePortName,
+		Baud: baudRate,
+	}
+
+	movePort, err = serial.OpenPort(config)
+	if err != nil {
+		log.Fatalf("cannot open port %s: %v\n", movePortName, err)
+	}
 }
 
 func main() {
-	ctx := &daemon.Context{
-		PidFileName: "roverd.pid",
-		PidFilePerm: 0644,
-		LogFileName: "roverd.log",
-		LogFilePerm: 0644,
-		WorkDir:     "./",
-		Umask:       027,
-	}
-	defer ctx.Release()
+	fmt.Println("started")
 
-	_, err := ctx.Reborn()
-	if err != nil {
-		log.Fatalln("failed to reborn context:", err)
-	}
-
-	log.Println("- - - - - - - - - - - - - - -")
-	log.Println("daemon started")
-
-	signal.Notify(signals, syscall.SIGTERM, syscall.SIGINT)
-	go listenSignals()
+	serveHTTP()
 }
 
-func listenSignals() {
-	for sig := range signals {
-		log.Printf("dameon stopped by %s\n", sig.String())
-		os.Exit(0)
+func serveHTTP() {
+	http.HandleFunc("/", handleIndex)
+	http.HandleFunc("/move", handleMove)
+	http.ListenAndServe(":"+port, nil)
+}
+
+func handleIndex(w http.ResponseWriter, req *http.Request) {
+	log.Printf("request from %s: %s %q", req.RemoteAddr, req.Method, req.URL)
+	fmt.Fprintln(w, "hello from roverd!")
+}
+
+func handleMove(w http.ResponseWriter, req *http.Request) {
+	b, err := io.ReadAll(req.Body)
+	if err != nil {
+		log.Fatalln("failed to read body:", err)
+	}
+
+	log.Printf("body: %s\n", b)
+
+	m := make(map[string]interface{})
+	err = json.Unmarshal(b, &m)
+	if err != nil {
+		log.Fatalln("failed to unmarshal:", err)
+	}
+
+	speed, ok := m["speed"].(float64)
+	if !ok {
+		log.Fatalln("failed to convert speed to float64")
+	}
+
+	data := []byte{'G', byte(speed)}
+	f := frames.Create([2]byte{'M', 'T'}, data)
+	_, err = movePort.Write(f)
+	if err != nil {
+		log.Fatalln("failed to write frame to port:", err)
+	}
+
+	if verbose {
+		for _, b := range f {
+			log.Println(frames.DescribeByte(b))
+		}
 	}
 }
